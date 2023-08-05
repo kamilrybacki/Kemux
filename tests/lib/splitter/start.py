d2a1b1a989632e5cbd16f1afdaee5c75fb102b1c
kamil.rybacki <kamil.rybacki@pg.edu.pl>
2023-08-05 10:37:47 +0200

    change env var names

--- tests/lib/splitter/start.py
+++ tests/lib/splitter/start.py
@@ -4,12 +4,12 @@ import kemux.logic.processing
 
 
 def start() -> None:
-    if not (streams_dir := os.getenv('KEMUX_STREAMS_DIR')):
-        raise ValueError('KEMUX_STREAMS_DIR environment variable not set')
-    if not (data_dir := os.getenv('KEMUX_DATA_DIR')):
-        raise ValueError('KEMUX_DATA_DIR environment variable not set')
-    if not (kafka_address := os.getenv('KEMUX_KAFKA_ADDRESS')):
-        raise ValueError('KEMUX_KAFKA_ADDRESS environment variable not set')
+    if not (streams_dir := os.getenv('STREAMS_DIR')):
+        raise ValueError('STREAMS_DIR environment variable not set')
+    if not (data_dir := os.getenv('DATA_DIR')):
+        raise ValueError('DATA_DIR environment variable not set')
+    if not (kafka_address := os.getenv('KAFKA_ADDRESS')):
+        raise ValueError('KAFKA_ADDRESS environment variable not set')
     receiver = kemux.logic.processing.Processor.init(
         kafka_address,
         data_dir,
