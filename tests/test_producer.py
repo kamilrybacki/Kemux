6ceeee52b7ac4bd35e0774e50070d869a9775560
kamil.rybacki <kamil.rybacki@pg.edu.pl>
2023-08-10 21:19:17 +0200

    add

--- tests/test_producer.py
+++ tests/test_producer.py
@@ -12,6 +12,7 @@ NUMBER_OF_SAMPLES = 20
 def test_for_consistency(tests_logger: logging.Logger, use_consumer: conftest.ConsumerFactory):
     consumer: kafka.KafkaConsumer = use_consumer()
     assert consumer.bootstrap_connected()
+    tests_logger.info("Connected to Kafka broker successfully")
 
     consumer.subscribe([lib.producer.start.TEST_TOPIC])
     for _ in range(NUMBER_OF_SAMPLES):
