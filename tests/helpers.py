3cd8fe58fe3558e07b3fc0909b1cbfe28012a7ce
kamil.rybacki <kamil.rybacki@pg.edu.pl>
2023-08-25 11:48:48 +0200

    filter to classes

--- tests/helpers.py
+++ tests/helpers.py
@@ -8,21 +8,13 @@ import lib.splitter.streams.secondary
 
 @functools.lru_cache
 def get_splitter_output_topics() -> list[str]:
-    logging.info([
-        stream.Outputs.__dict__.values()
-        for stream in [
-            lib.splitter.streams.primary,
-            lib.splitter.streams.secondary,
-        ]
-    ]
-    )
-
     return [
         topic
         for sublist in [
             [
                 output.IO.topic
                 for output in stream.Outputs.__dict__.values()
+                if isinstance(output, type)
             ]
             for stream in [
                 lib.splitter.streams.primary,
