37d251fc91f3d34b5b58c0d00beeb3e195eb2569
kamil.rybacki <kamil.rybacki@pg.edu.pl>
2023-08-25 11:51:32 +0200

    filter outputs

--- tests/helpers.py
+++ tests/helpers.py
@@ -5,6 +5,9 @@ import functools
 import lib.splitter.streams.primary
 import lib.splitter.streams.secondary
 
+import kemux.data.schema.output
+import kemux.data.io.output
+
 
 @functools.lru_cache
 def get_splitter_output_topics() -> list[str]:
@@ -14,7 +17,7 @@ def get_splitter_output_topics() -> list[str]:
             [
                 output.IO.topic
                 for output in stream.Outputs.__dict__.values()
-                if isinstance(output, type)
+                if hasattr(output, 'IO') and hasattr(output, 'Schema')
             ]
             for stream in [
                 lib.splitter.streams.primary,
