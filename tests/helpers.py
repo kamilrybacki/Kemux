129966e2aae605e11251c31d7ab404bcf8d60081
kamil.rybacki <kamil.rybacki@pg.edu.pl>
2023-08-25 11:42:58 +0200

    pritn list

--- tests/helpers.py
+++ tests/helpers.py
@@ -8,12 +8,13 @@ import lib.splitter.streams.secondary
 
 @functools.lru_cache
 def get_splitter_output_topics() -> list[str]:
-    logging.info(
+    logging.info([
         stream.Outputs.__dict__.values()
         for stream in [
             lib.splitter.streams.primary,
             lib.splitter.streams.secondary,
         ]
+    ]
     )
 
     return [
