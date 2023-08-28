906554c760e4bdb367c10fe815433e8bed29c9be
kamil.rybacki <kamil.rybacki@pg.edu.pl>
2023-08-28 10:20:02 +0200

    print msg

--- kemux/data/io/input.py
+++ kemux/data/io/input.py
@@ -9,4 +9,3 @@ class StreamInput(kemux.data.io.base.IOBase):
     @staticmethod
     def ingest(message: dict) -> dict:
         raise NotImplementedError(f'{__name__}.recieve() must be implemented!')
-
--- kemux/data/stream.py
+++ kemux/data/stream.py
@@ -11,6 +11,7 @@ class StreamBase:
     outputs: list[kemux.data.io.output.StreamOutput]
 
     async def process(self, message: kemux.data.schema.input.InputRecordT) -> None:
+        self.input.logger.info(message.asdict())
         message._validate()
         raw_message = message.to_dict()
         ingested_message = self.input.ingest(raw_message)
