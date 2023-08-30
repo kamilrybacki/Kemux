b374d92517094451e294047b0c85a09ade522a24
kamil.rybacki <kamil.rybacki@pg.edu.pl>
2023-08-30 14:54:04 +0200

    add manual streams loading

--- kemux/logic/processing.py
+++ kemux/logic/processing.py
@@ -22,59 +22,70 @@ import kemux.data.stream
 
 DEFAULT_MODELS_PATH = 'streams'
 
+StreamsMap = dict[str, kemux.data.stream.StreamBase]
+
 
 @dataclasses.dataclass(kw_only=True)
 class Processor:
     kafka_address: str
-    streams_dir: str
+    streams_dir: str | None
     persistent_data_directory: str
+    logger: logging.Logger = dataclasses.field(init=False, default=logging.getLogger(__name__))
+    agents: dict[str, faust.types.AgentT] = dataclasses.field(init=False, default_factory=dict)
+
     _app: faust.App = dataclasses.field(init=False)
+
     __instance: Processor | None = dataclasses.field(init=False, default=None)
-    __logger: logging.Logger = dataclasses.field(init=False, default=logging.getLogger(__name__))
-    __streams: dict[str, kemux.data.stream.StreamBase] = dataclasses.field(init=False, default_factory=dict)
-    __agents: dict[str, faust.types.AgentT] = dataclasses.field(init=False, default_factory=dict)
+
+    @property
+    def streams(self) -> StreamsMap:
+        return self.__streams
+
+    @streams.setter
+    def streams(self, streams: StreamsMap) -> None:
+        self.__streams = self.order_streams(streams)
 
     @classmethod
-    def init(cls, kafka_address: str, data_dir: str, streams_dir: str) -> Processor:
+    def init(cls, kafka_address: str, data_dir: str, streams_dir: str | None = None) -> Processor:
         if cls.__instance is None:
             instance: Processor = cls(
                 kafka_address=kafka_address,
                 streams_dir=streams_dir,
                 persistent_data_directory=data_dir,
             )
-            instance.__logger.info('Initialized receiver')
+            instance.logger.info('Initialized receiver')
             logging.basicConfig(
                 level=logging.INFO,
                 format='%(asctime)s %(levelname)s %(message)s',
                 datefmt='%Y-%m-%d %H:%M:%S',
             )
-            instance.__logger.info(f'Connecting to Kafka broker: {kafka_address}')
+            instance.logger.info(f'Connecting to Kafka broker: {kafka_address}')
             app = faust.App(
                 'splitter_broker',
                 broker=kafka_address,
                 value_serializer='json',
                 datadir=instance.persistent_data_directory,
             )
-            instance.__streams = instance.load_streams()  # pylint: disable=unused-private-member
+            instance.streams = instance.load_streams() if streams_dir else {}
             instance._app = app
             cls.__instance = instance
         return cls.__instance
 
-    def load_streams(self) -> dict[str, kemux.data.stream.StreamBase]:
-        if not os.path.isdir(self.streams_dir):
+    def load_streams(self) -> StreamsMap:
+        if not os.path.isdir(self.streams_dir):  # type: ignore
             raise ValueError(f'Invalid streams directory: {self.streams_dir}')
         present_modules_filenames: filter[str] = filter(
             lambda module: module.endswith('.py'),
             os.listdir(self.streams_dir),
         )
         return {
-            module_filename.removesuffix('.py'): self._load_stream_module(module_filename)
+            module_filename.removesuffix('.py'): self.load_stream_module(module_filename)
             for module_filename in present_modules_filenames
         }
 
-    def _load_stream_module(self, module_filename: str) -> kemux.data.stream.StreamBase:
+    def load_stream_module(self, module_filename: str) -> kemux.data.stream.StreamBase:
         module_name = module_filename.removesuffix('.py')
-        module_full_path = os.path.join(self.streams_dir, module_filename)
+        module_full_path = os.path.join(self.streams_dir, module_filename)  # type: ignore
         try:
             imported_module: types.ModuleType = importlib.machinery.SourceFileLoader(module_name, module_full_path).load_module()  # pylint: disable=deprecated-method, no-value-for-parameter
         except (OSError, ImportError) as cant_import_stream_module:
@@ -84,38 +95,38 @@ class Processor:
         if not (stream_outputs := getattr(imported_module, 'Outputs', None)):
             raise ValueError(f'No outputs found for stream module: {module_name}')
         return kemux.data.stream.StreamBase(
-            input=self._load_input(stream_input),
-            outputs=self._load_outputs(stream_outputs)
+            input=self.load_input(stream_input),
+            outputs=self.load_outputs(stream_outputs)
         )
 
-    def _load_input(self, input_class: type) -> kemux.data.io.input.StreamInput:
+    def load_input(self, input_class: type) -> kemux.data.io.input.StreamInput:
         input_schema: kemux.data.schema.input.InputSchema
         input_io: kemux.data.io.input.StreamInput
 
-        input_schema, input_io = self._extract_schema_and_io(input_class)  # type: ignore
+        input_schema, input_io = self.extract_schema_and_io(input_class)  # type: ignore
         input_schema.find_decorated_fields()
         input_schema.construct_input_record_class()
         input_io.schema = input_schema
         return input_io
 
-    def _load_outputs(self, outputs: type) -> list[kemux.data.io.output.StreamOutput]:
+    def load_outputs(self, outputs: type) -> list[kemux.data.io.output.StreamOutput]:
         return [
-            self._load_output(output)
+            self.load_output(output)
             for output in outputs.__dict__.values()
             if inspect.isclass(output)
         ]
 
-    def _load_output(self, output_class: type) -> kemux.data.io.output.StreamOutput:
+    def load_output(self, output_class: type) -> kemux.data.io.output.StreamOutput:
         output_schema: kemux.data.schema.output.OutputSchema
         output_io: kemux.data.io.output.StreamOutput
 
-        output_schema, output_io = self._extract_schema_and_io(output_class)  # type: ignore
+        output_schema, output_io = self.extract_schema_and_io(output_class)  # type: ignore
         output_schema.find_decorated_fields()
         output_schema.construct_output_record_class()
         output_io.schema = output_schema
         return output_io
 
-    def _extract_schema_and_io(self, source: type) -> tuple[
+    def extract_schema_and_io(self, source: type) -> tuple[
         kemux.data.schema.base.SchemaBase,
         kemux.data.io.base.IOBase
     ]:
@@ -130,13 +141,29 @@ class Processor:
             raise ValueError(f'Invalid input {source.__name__} - no io found')
         return schema, io
 
+    def add_stream(self, name: str, stream_input_class: type, stream_outputs_class: type) -> None:
+        stream_input = self.load_input(stream_input_class)
+        stream_outputs = self.load_outputs(stream_outputs_class)
+        self.streams[name] = kemux.data.stream.StreamBase(
+            input=stream_input,
+            outputs=stream_outputs,
+        )
+
+    def remove_stream(self, name: str) -> None:
+        if name not in self.streams:
+            self.logger.warning(f'No stream found with name: {name}')
+            return
+        del self.streams[name]
+
     def start(self) -> None:
-        self.__logger.info('Starting receiver')
+        if not self.streams.keys():
+            raise ValueError('No streams have been loaded!')
+
+        self.logger.info('Starting receiver')
         stream: kemux.data.stream.StreamBase
-        sorted_streams = self.order_streams()
-        for stream_name, stream in sorted_streams.items():
+        for stream_name, stream in self.streams.items():
             stream_input: kemux.data.io.input.StreamInput = stream.input
-            self.__logger.info(f'{stream_name}: activating input topic handler')
+            self.logger.info(f'{stream_name}: activating input topic handler')
             stream_input.initialize_handler(self._app)
 
             output: kemux.data.io.output.StreamOutput
@@ -155,7 +182,7 @@ class Processor:
 
             # pylint: disable=cell-var-from-loop
             async def _process_input_stream_message(messages: faust.StreamT[kemux.data.schema.input.InputSchema]) -> None:
-                self.__logger.info(f'{stream_name}: activating output topic handlers')
+                self.logger.info(f'{stream_name}: activating output topic handlers')
                 async for message in messages:
                     await stream.process(message)  # type: ignore
 
@@ -163,26 +190,25 @@ class Processor:
             if not input_topics_handler:
                 raise ValueError(f'Invalid input stream topic handler: {stream_name}')
 
-            self.__logger.info(f'{stream_name}: activating stream agent')
-            self.__agents[stream_name] = self._app.agent(input_topics_handler)(_process_input_stream_message)
+            self.logger.info(f'{stream_name}: activating stream agent')
+            self.agents[stream_name] = self._app.agent(input_topics_handler)(_process_input_stream_message)
         self._app.main()
 
-    def order_streams(self) -> dict[str, kemux.data.stream.StreamBase]:
-        streams_info: list[tuple] = [
+    def order_streams(self, streams: StreamsMap) -> StreamsMap:
+        ordered_streams = {}
+        for stream_info in self.finds_streams_order([
             stream.topics()
-            for stream in self.__streams.values()
-        ]
-        sorted_streams = {}
-        for stream_info in self.sort_streams_info(streams_info):
+            for stream in streams.values()
+        ]):
             input_topic = stream_info[0]
-            for stream_name, stream in self.__streams.items():
+            for stream_name, stream in streams.items():
                 if stream.input.topic == input_topic:
-                    sorted_streams[stream_name] = stream
+                    ordered_streams[stream_name] = stream
                     break
-        return sorted_streams
+        return ordered_streams
 
     @staticmethod
-    def sort_streams_info(info: list[tuple]) -> list[tuple]:
+    def finds_streams_order(info: list[tuple]) -> list[tuple]:
         for stream_index in range(len(info)):
             stream_input_topic = info[stream_index][0]
             for other_stream_index in range(stream_index + 1, len(info)):
