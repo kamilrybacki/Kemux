dcd2c8ffbe91b54cdf51b40f1eca7c2a8cbcad0e
kamil.rybacki <kamil.rybacki@pg.edu.pl>
2023-09-01 10:33:03 +0200

    initialize in async function

--- kemux/data/io/output.py
+++ kemux/data/io/output.py
@@ -16,22 +16,16 @@ class StreamOutput(kemux.data.io.base.IOBase):
     @classmethod
     async def send(cls, message: dict) -> None:
         transformed_message = cls.schema.transform(message=message)  # type: ignore
-        cls.logger.info(f'Sending message: {transformed_message}')
+        cls.logger.info(f'Sending message: {transformed_message} to {cls.topic}')
         if cls.schema.validate(message=transformed_message):  # type: ignore
             await cls.topic_handler.send(value=transformed_message)  # type: ignore
         else:
             cls.logger.warning(f'Invalid message: {message}')
 
     @classmethod
-    def declare(cls) -> None:
-        kemux.logic.concurrency.try_in_event_loop(
-            cls.__declare
-        )
-        cls.logger.info(f'Output topic declared: {cls.topic}')
-
-    @classmethod
-    async def __declare(cls) -> None:
+    async def declare(cls) -> None:
         await cls.topic_handler.declare()
         await cls.topic_handler.send(
             value='__kemux_init__'
         )
+        cls.logger.info(f'Output topic declared: {cls.topic}')
