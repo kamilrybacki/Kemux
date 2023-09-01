391d79453453a5877b1dc1d4cb4a93155f647b21
kamil.rybacki <kamil.rybacki@pg.edu.pl>
2023-09-01 17:24:59 +0200

    fixp aths

--- tests/helpers.py
+++ tests/helpers.py
@@ -17,8 +17,8 @@ def get_splitter_output_topics() -> list[str]:
                 if hasattr(output, 'IO') and hasattr(output, 'Schema')
             ]
             for stream in [
-                lib.splitter.streams.primary,
-                lib.splitter.streams.secondary,
+                streams.primary,
+                streams.secondary,
             ]
         ]
         for topic in sublist
@@ -30,7 +30,7 @@ def get_filtering_function_for_topic(topic: str) -> typing.Callable[[dict], bool
     try:
         outputs_class = getattr(
             getattr(
-                lib.splitter.streams.primary,
+                streams.primary,
                 'Outputs',
             ),
             topic
@@ -38,7 +38,7 @@ def get_filtering_function_for_topic(topic: str) -> typing.Callable[[dict], bool
     except AttributeError:
         outputs_class = getattr(
             getattr(
-                lib.splitter.streams.secondary,
+                streams.secondary,
                 'Outputs',
             ),
             topic
