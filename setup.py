b7c6d27b1393c528fafacec15c37b2372814bebf
kamil.rybacki <kamil.rybacki@pg.edu.pl>
2023-09-03 21:31:01 +0200

    python version

--- setup.py
+++ setup.py
@@ -4,19 +4,20 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-version = os.environ.get("RELEASE_VERSION", "0.0.0.dev0")
-
-setuptools.setup(
-    name="kemux",
-    version=version,
-    author="Kamil Rybacki",
-    author_email="kamilandrzejrybacki@gmail.com",
-    description="Python interface for splitting and/or merging Kafka topics",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://kemux.readthedocs.io/en/latest",
-    packages=setuptools.find_packages(),
-    install_requires=[
-        "faust-streaming==0.10.14",
-    ]
-)
+if version := os.environ.get("RELEASE_VERSION"):
+    setuptools.setup(
+        name="kemux",
+        version=version,
+        author="Kamil Rybacki",
+        author_email="kamilandrzejrybacki@gmail.com",
+        description="Python interface for splitting and/or merging Kafka topics",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://kemux.readthedocs.io/en/latest",
+        packages=setuptools.find_packages(),
+        install_requires=[
+            "faust-streaming==0.10.14",
+        ]
+    )
+else:
+    raise RuntimeError("RELEASE_VERSION environment variable is not set")
